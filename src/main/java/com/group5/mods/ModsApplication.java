54b7f600aebd17bc83bfd0ccec818230323c5a90
Jasraj Singh <elijahbaggins3@gmail.com>
2023-02-02 00:45:03 +0000

    Created new project with all required dependencies

---
+++ src/main/java/com/group5/mods/ModsApplication.java
@@ -0,0 +1,13 @@
+package com.group5.mods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsApplication.class, args);
+	}
+
+}
