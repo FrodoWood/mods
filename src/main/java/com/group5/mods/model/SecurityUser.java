d5d0e6c701255c480355f6c5e047407fe6d20492
Jasraj Singh <elijahbaggins3@gmail.com>
2023-02-04 23:53:02 +0000

    Added SecurityUser implementing UserDetails, overriden methods

---
+++ src/main/java/com/group5/mods/model/SecurityUser.java
@@ -0,0 +1,56 @@
+package com.group5.mods.model;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUser implements UserDetails {
+
+    private User user;
+
+    public SecurityUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // List<SimpleGrantedAuthority> simpleGrantedAuthorityList = new ArrayList<>();
+        // simpleGrantedAuthorityList.add(new SimpleGrantedAuthority(user.getRole()));
+        // return simpleGrantedAuthorityList;
+        return Arrays.asList(new SimpleGrantedAuthority(user.getRoles()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
