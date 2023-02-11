85827741953071ea5c5ea8735c52f6019a4e5ed3
JackWojtowicz <jackwojtowicz02@gmail.com>
2023-02-11 21:15:51 +0000

    Individual product page
    Individual product html file as well as a controller to go with it

---
+++ src/main/java/com/group5/mods/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.group5.mods.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/product")
+    public String product(Model model) {
+        return "product";
+    }
+}
