ee0e41288c80ccda897f1d0e923be54b12eb35cf
Jasraj Singh <elijahbaggins3@gmail.com>
2023-02-12 03:41:39 +0000

    Fixed migration typo, use ` ` instead of ' '

--- src/main/resources/db/migration/V3__create_products_table.sql
+++ src/main/resources/db/migration/V3__create_products_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `products` (
 
     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(20) NOT NULL,
-    'rating' int NOT NULL,
-    'description' varchar(256) NOT NULL
+    `rating` int NOT NULL,
+    `description` varchar(256) NOT NULL
 )
