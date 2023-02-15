21ecd931c5b4d9b155b37b262cd0a95abe8312a2
JackWojtowicz <jackwojtowicz02@gmail.com>
2023-02-15 09:54:02 +0000

    Products showing in page
    HTML and repo code to show products on the web page

--- src/main/java/com/group5/mods/controller/StoreController.java
+++ src/main/java/com/group5/mods/controller/StoreController.java
@@ -1,13 +1,21 @@
 package com.group5.mods.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.group5.mods.repository.ProductRepository;
 
 @Controller
 public class StoreController {
+
+    @Autowired
+    ProductRepository productRepository;
+
     @GetMapping("/store")
     public String store(Model model) {
+        model.addAttribute("products", productRepository.findAll());
         return "store";
     }
 }
--- src/main/java/com/group5/mods/repository/ProductRepository.java
+++ src/main/java/com/group5/mods/repository/ProductRepository.java
@@ -1,7 +1,5 @@
 package com.group5.mods.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
