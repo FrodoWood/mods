21fbcf55c53a8149f894a420191843c82da81db4
JackWojtowicz <jackwojtowicz02@gmail.com>
2023-02-15 11:41:03 +0000

    Product page
    Page for individual product

--- src/main/java/com/group5/mods/controller/ProductController.java
+++ src/main/java/com/group5/mods/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.group5.mods.controller;
 
+import org.hibernate.metamodel.mapping.internal.IdClassEmbeddable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.group5.mods.repository.ProductRepository;
 
 @Controller
@@ -11,8 +14,9 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping("/product")
-    public String product(Model model) {
+    @GetMapping("/prduct/{id}")
+    public String product(@PathVariable("id") long id, Model model) {
+        model.addAttribute("product", productRepository.findById(id));
         return "product";
     }
 }
