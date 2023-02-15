b088bbe6eadc8c276e121755fcfb69de23e57bba
Jasraj Singh <elijahbaggins3@gmail.com>
2023-02-15 11:28:06 +0000

    Added 2 test products

---
+++ src/main/resources/db/migration/V5__add_test_products.sql
@@ -0,0 +1,4 @@
+INSERT INTO `products` (`id`, `name`, `rating`, `description`, `make`, `model`, `category`, `price`) 
+VALUES 
+(NULL, 'radiator name', '4', 'This is a description', 'AUDI', 'A5', 'radiators', '80.80'), 
+(NULL, 'engine name', '3', 'engine description', 'BMW', 'X5', 'engine', '500.34')
