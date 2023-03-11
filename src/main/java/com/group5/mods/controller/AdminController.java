561fe8b48baef7ca16ade58db2e1c39971b62d96
Jasraj Singh <elijahbaggins3@gmail.com>
2023-03-11 03:23:56 +0000

    WIP Added admin form to add new product

--- src/main/java/com/group5/mods/controller/AdminController.java
+++ src/main/java/com/group5/mods/controller/AdminController.java
@@ -1,13 +1,18 @@
 package com.group5.mods.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -89,5 +94,58 @@ public class AdminController {
         userRepository.save(user.get());
 
         return "redirect:/admin/users";
+    }
+
+    @GetMapping("/admin/products/add")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String showAddProduct(Model model) {
+        Map<String, List<String>> makeModelMap = new HashMap<>();
+        makeModelMap.put("Audi", Arrays.asList("A5", "Q5", "S5"));
+        makeModelMap.put("BMW", Arrays.asList("Z4", "X5", "M4"));
+
+        Set<String> makeSet = makeModelMap.keySet();
+        model.addAttribute("makeModelMap", makeModelMap);
+        model.addAttribute("makeSet", makeSet);
+        model.addAttribute("product", new Product());
+        
+        return "admin/admin_addProduct";
+    }
+
+    @PostMapping("/admin/products/addProduct")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String addProduct(@ModelAttribute("product") Product product, BindingResult result, Model model) {
+        Map<String, List<String>> makeModelMap = new HashMap<>();
+        makeModelMap.put("Audi", Arrays.asList("A5", "Q5", "S5"));
+        makeModelMap.put("BMW", Arrays.asList("Z4", "X5", "M4"));
+        Set<String> makeSet = makeModelMap.keySet();
+
+        if(product.getModel() == null || product.getModel().isEmpty()){
+            List<String> models = makeModelMap.get(product.getMake());
+            model.addAttribute("models", models);
+        }else{
+            // User has correctly selected the make and model, now save the product to the database
+            return "redirect:/admin/products";
+        }
+
+        model.addAttribute("makeSet", makeSet);
+        model.addAttribute("makeModelMap", makeModelMap);
+        return "/admin/admin_addProduct";
+    }
+
+    @PostMapping("/admin/products/updateAddProductForm")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String updateAddProductForm(@ModelAttribute("product") Product product, BindingResult result, Model model) {
+        Map<String, List<String>> makeModelMap = new HashMap<>();
+        makeModelMap.put("Audi", Arrays.asList("A5", "Q5", "S5"));
+        makeModelMap.put("BMW", Arrays.asList("Z4", "X5", "M4"));
+        Set<String> makeSet = makeModelMap.keySet();
+        
+        List<String> models = makeModelMap.get(product.getMake());
+        model.addAttribute("models", models);
+        
+
+        model.addAttribute("makeSet", makeSet);
+        model.addAttribute("makeModelMap", makeModelMap);
+        return "/admin/admin_addProduct";
     }
 }
